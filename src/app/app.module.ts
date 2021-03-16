b06cf8b59a1c912d2a8179c8f057d1c8b3fb20de
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-03-16 15:46:12 -0300

    Aula do dia 11/02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ModalComponent } from './components/modal/modal.component';
+import { HeaderComponent } from './@shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent
+    ModalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
